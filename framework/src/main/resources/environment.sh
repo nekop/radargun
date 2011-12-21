3cc34d8a675353cd748525ab47a89e20397b6a9b
Takayoshi Kimura <tkimura@redhat.com>
2011-12-21 17:27:17 +0900

    Tuning Java VM options

--- framework/src/main/resources/environment.sh
+++ framework/src/main/resources/environment.sh
@@ -11,7 +11,9 @@
 
 BIND_ADDRESS=${MY_BIND_ADDRESS}
 JG_FLAGS="-Dresolve.dns=false -Djgroups.timer.num_threads=4"
-JVM_OPTS="-server -Xmx1024M -Xms1024M"
+# JVM_OPTS="-server -Xmx1024M -Xms1024M"
+JVM_OPTS="-Xmn256m -Xms1024m -Xmx1024m -XX:MaxPermSize=256m -Xss512k -Xverify:none -XX:+UseConcMarkSweepGC -XX:+TieredCompilation -XX:+UseCompressedOops"
+JVM_OPTS="$JVM_OPTS -Xloggc:gc.log -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -XX:+PrintClassHistogram -XX:+HeapDumpOnOutOfMemoryError"
 JVM_OPTS="$JVM_OPTS $JG_FLAGS"
 JPROFILER_HOME=${HOME}/jprofiler6
 JPROFILER_CFG_ID=103
